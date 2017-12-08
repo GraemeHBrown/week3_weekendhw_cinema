8615c55c3365f52d4d5e8a12601be34e505af4aa
Graeme Brown <graemehbrown@googlemail.com>
2017-12-08 14:29:46 +0000

    film.rb added with Film class.

---
+++ models/film.rb
@@ -0,0 +1,12 @@
+require_relative('../db/sql_runner.rb')
+
+class Film
+
+  def initialize(options)
+    @id = options['id'].to_i
+    @title = options['title']
+    @price = options['price'].to_f
+  end
+
+
+end
