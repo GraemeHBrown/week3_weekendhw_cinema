4a3952948df4fe427a7a379a9727ab1512466755
Graeme Brown <graemehbrown@googlemail.com>
2017-12-08 15:06:21 +0000

    Added Ticket class.

--- console.rb
+++ console.rb
@@ -2,12 +2,14 @@ require_relative('./models/customer.rb')
 require_relative('./models/film.rb')
 require('pry-byebug')
 
-
+#Saving objects to db
 customer1 = Customer.new({'name' => 'Fred', 'funds' => 20.00})
 customer1.save()
 
 film1 = Film.new({'title' => 'Lord of the Rings', 'price' => 8.25})
 film1.save()
+
+
 
 binding.pry
 nil
