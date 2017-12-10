ff5aa76b9d1df1a9e6c4499463035b4182474874
Graeme Brown <graemehbrown@googlemail.com>
2017-12-10 12:48:55 +0000

    Alterred Ticket class to use screening_id from neww Screenings relation instead of film_id.  Chenged methods in Tickets as necessary.

--- models/ticket.rb
+++ models/ticket.rb
@@ -3,11 +3,11 @@ require_relative('../db/sql_runner.rb')
 class Ticket
 
   attr_reader :id
-  attr_accessor :customer_id, :film_id
+  attr_accessor :customer_id, :screening_id
   def initialize(options)
     @id = options['id'].to_i
     @customer_id = options['customer_id'].to_i
-    @film_id = options['film_id'].to_i
+    @screening_id = options['screening_id'].to_i
   end
 
   def Ticket.all()
@@ -34,14 +34,14 @@ class Ticket
     sql = "INSERT INTO tickets
     (
       customer_id,
-      film_id
+      screening_id
     )
     VALUES
     (
       $1, $2
     )
     RETURNING id"
-    values = [@customer_id, @film_id]
+    values = [@customer_id, @screening_id]
     ticket = SqlRunner.run(sql, values).first
     @id = ticket['id'].to_i
   end
@@ -57,13 +57,13 @@ class Ticket
     sql = "UPDATE tickets
     SET (
       customer_id,
-      film_id
+      screening_id
       ) =
       (
         $1, $2
       )
       WHERE id = $3"
-      values = [@customer_id, @film_id, @id]
+      values = [@customer_id, @screening_id, @id]
       SqlRunner.run(sql, values)
     end
 
